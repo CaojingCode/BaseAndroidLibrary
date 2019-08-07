486570e3936b2d6552fe53994788329395d7179e
caojing <641809386@qq.com>
2019-08-08 00:22:09 +0800

    多布局拖拽初步

---
+++ app/src/main/java/com/kotlin/library/TZAdapter.kt
@@ -0,0 +1,33 @@
+package com.kotlin.library
+
+import com.chad.library.adapter.base.BaseItemDraggableAdapter
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.chad.library.adapter.base.util.MultiTypeDelegate
+
+class TZAdapter : BaseItemDraggableAdapter<TZBean, BaseViewHolder>(null) {
+
+    init {
+        multiTypeDelegate = object : MultiTypeDelegate<TZBean>() {
+            override fun getItemType(t: TZBean): Int {
+                return t.type
+            }
+
+        }
+        multiTypeDelegate.registerItemType(1, R.layout.item_text)
+            .registerItemType(2, R.layout.item_image)
+    }
+
+    override fun convert(helper: BaseViewHolder, item: TZBean?) {
+        when (helper.itemViewType) {
+            1 -> {
+
+            }
+            2 -> {
+
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/kotlin/library/TZBean.kt
@@ -0,0 +1,7 @@
+package com.kotlin.library
+
+data class TZBean(
+    val type: Int = 0,
+    val text: String = "",
+    val imageUrl: String = ""
+)
